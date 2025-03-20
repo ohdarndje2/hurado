14ed5ddc4c93d315084aa94ca629f39aa74d1879
kodename-kian <kodename.kian@gmail.com>
2025-03-20 02:03:08 -0700

    Lint fix

--- src/server/authorization.ts
+++ src/server/authorization.ts
@@ -29,7 +29,11 @@ export function canManageContests(session: SessionData | null): boolean {
 }
 
 export function canManageUser(session: SessionData | null, id: string): boolean {
-  if (session == null) { return false; }
-  else if (session.user.role == 'admin') { return true; }
-  else { return (session.user.id == id); }
+  if (session == null) { 
+    return false; 
+  } else if (session.user.role == 'admin') { 
+    return true; 
+  } else { 
+    return (session.user.id == id); 
+  }
 }
